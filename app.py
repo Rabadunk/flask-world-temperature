21d834bb7c9be183c744a78c988ec8d6683bbfe7
Jonathan Alphonso <31195359+Rabadunk@users.noreply.github.com>
2018-01-28 12:21:53 +1300

    UI and UX edits
    The previous version required the use of zipcode to track where the user wanted the weather. I have replaced this with city and country inputs instead of zipcode allowing more variety and simplicity because who actually remembers zipcodes? Also i've made it a tad prettier using some bootstrap and css3.

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ from flask import Flask, render_template, request
 #Importing requests to request from url
 import requests
 
+#Importing ast to convert str to dict
+import ast
+f = open("countries.txt", "r")
+data = f.read()
+countries = ast.literal_eval(data)
+f.close
+
 app = Flask(__name__)
 
 @app.route('/temperature', methods=['POST'])
 def temperature():
-    zipcode = request.form['zip']
-    response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+ zipcode +',nz&appid=0cd5498b4fc08eb51125e83318e058ef')
+    city = request.form['city']
+    country = request.form['country']
+    countryCode = countries[country]
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+ city + ',' + countryCode + '&appid=0cd5498b4fc08eb51125e83318e058ef')
     json_object = response.json()
     temp_k = float(json_object['main']['temp'])
     temp_c = round(temp_k - 273.15, 2)
-    return render_template('temperature.html', temp = temp_c)
+    return render_template('temperature.html', temp=temp_c, city= city, country=country)
 
 @app.route('/')
 def index():
